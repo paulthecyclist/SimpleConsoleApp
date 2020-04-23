e00814fa2669fd01e832065e603d30f89fd1a708
Paul Davies <pauldavies129@gmail.com>
2020-04-23 09:01:41 +0100

    Trying to get £ test to run

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using SimpleConsoleApp;
 using Xunit;
 
@@ -6,6 +7,11 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        public UnitTest1()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
+        }
+
         [Fact]
         public void GivenTwoNumbers_WhenAdded_ThenCorrectAnswer()
         {
@@ -15,6 +21,30 @@ namespace XUnitTestProject1
             var actual = calculator.Add(10, 5);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CurrencyTest1()
+        {
+            var expected = @"A Short stay from 07/09/2017 16:50:00 to 09/09/2017 19:15:00 would cost £12.28";
+            
+            //From spec
+            var actual = $"A Short stay from 07/09/2017 16:50:00 to 09/09/2017 19:15:00 would cost {12.28:C}";
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CurrencyTest2()
+        {
+            var expected = @"A Short stay from 07/09/2017 16:50:00 to 09/09/2017 19:15:00 would cost £12.28";
+
+            //From spec
+            var actual = $"A Short stay from 07/09/2017 16:50:00 to 09/09/2017 19:15:00 would cost {12.28:C}";
+
+            var stringsMatch = expected.Equals(actual, StringComparison.CurrentCulture);
+
+            Assert.True(stringsMatch);
         }
     }
 }
